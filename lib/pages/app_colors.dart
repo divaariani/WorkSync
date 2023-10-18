d3fffc377c156545854b72f55ef613d5773ac013
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-10-18 15:46:19 +0700

    Add files via upload

---
+++ lib/pages/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color greenOne = Color(0xFFFBFBEB);
+  static const Color greenTwo = Color(0xFFE2E2D4);
+  static const Color greenThree = Color(0xFFC9C9BC);
+  static const Color greenFour = Color(0xFFA3A397);
+  static const Color greenFive = Color(0xFF76745C);
+}
