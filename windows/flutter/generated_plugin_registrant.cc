b702a928583fa13027e11b043f8b66b1091f95b7
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-10-26 13:24:24 +0700

    import geolocator for windows

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <geolocator_windows/geolocator_windows.h>
 #include <tflite_flutter_plus/tflite_flutter_plus_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  GeolocatorWindowsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("GeolocatorWindows"));
   TfliteFlutterPlusPluginCApiRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("TfliteFlutterPlusPluginCApi"));
 }
