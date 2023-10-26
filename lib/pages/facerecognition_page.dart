1819f134fcde05b959508dd4db2faa42ba5604b7
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-10-26 13:28:05 +0700

    implement face and location detector for attendance

--- lib/pages/facerecognition_page.dart
+++ lib/pages/facerecognition_page.dart
@@ -8,9 +8,15 @@ import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:quiver/collection.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:geolocator/geolocator.dart' as geolocator;
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+import 'app_colors.dart';
+import 'attendanceform_page.dart';
 import '../modules/detector.dart';
 import '../modules/utils.dart';
 import '../modules/model.dart';
+import '../utils/globals.dart';
 
 class FaceRecognitionPage extends StatefulWidget {
   const FaceRecognitionPage({Key? key}) : super(key: key);
@@ -20,13 +26,14 @@ class FaceRecognitionPage extends StatefulWidget {
 }
 
 class _FaceRecognitionPageState extends State<FaceRecognitionPage> with WidgetsBindingObserver {
+
+  // FACE DETECTION
   @override
   void initState() {
     super.initState();
     WidgetsBinding.instance!.addObserver(this);
     SystemChrome.setPreferredOrientations(
         [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
-
     _start();
   }
 
@@ -66,7 +73,6 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> with WidgetsB
 
   void initialCamera() async {
     CameraDescription description = await getCamera(CameraLensDirection.front);
-
     _camera = CameraController(
       description,
       ResolutionPreset.low,
@@ -166,71 +172,182 @@ class _FaceRecognitionPageState extends State<FaceRecognitionPage> with WidgetsB
     return _predRes;
   }
 
+  // LOCATION DETECT
+  String message = 'Location';
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled =
+        await geolocator.Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    geolocator.LocationPermission permission =
+        await geolocator.Geolocator.checkPermission();
+    if (permission == geolocator.LocationPermission.denied) {
+      permission = await geolocator.Geolocator.requestPermission();
+      if (permission == geolocator.LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == geolocator.LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cant request');
+    }
+
+    final position = await geolocator.Geolocator.getCurrentPosition();
+    globalLat = position.latitude.toString();
+    globalLong = position.longitude.toString();
+    await getLocationName();
+
+    setState(() {
+      message = 'Latitude $globalLat, Longitude: $globalLong';
+    });
+
+    return await geolocator.Geolocator.getCurrentPosition();
+  }
+
+  Future<String> getLocationName() async {
+    double latitude = double.parse(globalLat);
+    double longitude = double.parse(globalLong);
+
+    List<Placemark> placemarks =
+        await placemarkFromCoordinates(latitude, longitude);
+
+    if (placemarks.isNotEmpty) {
+      Placemark placemark = placemarks[0];
+      String locationName = placemark.name ?? "";
+      String thoroughfare = placemark.thoroughfare ?? "";
+      String subLocality = placemark.subLocality ?? "";
+      String locality = placemark.locality ?? "";
+      String administrativeArea = placemark.administrativeArea ?? "";
+      String country = placemark.country ?? "";
+      String postalCode = placemark.postalCode ?? "";
+
+      String address =
+          "$locationName $thoroughfare $subLocality $locality $administrativeArea $country $postalCode";
+
+      globalLocationName = address;
+      return address;
+    } else {
+      globalLocationName = "Location not found";
+      return "Location not found";
+    }
+  }
+
+  void _liveLocation() {
+    geolocator.LocationSettings locationSettings = geolocator.LocationSettings(
+        accuracy: geolocator.LocationAccuracy.high, distanceFilter: 1000);
+
+    geolocator.Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((geolocator.Position position) {
+      globalLat = position.latitude.toString();
+      globalLong = position.longitude.toString();
+
+      setState(() {
+        message = 'Latitude $globalLat, Longitude: $globalLong';
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('Face Recognition'),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            showDialog(
-                context: context,
-                builder: (context) {
-                  return AlertDialog(
-                    content: Column(children: [
-                      TextField(
-                        controller: _name,
-                      ),
-                      ElevatedButton(
-                          onPressed: () async {
-                            Navigator.pop(context);
-                            await Future.delayed(const Duration(milliseconds: 400));
-                            data[_name.text] = e1;
-                            jsonFile.writeAsStringSync(json.encode(data));
-
-                            if (_camera != null) {
-                              await _camera!.stopImageStream();
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('Face Recognition'),
+        ),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Column(children: [
+                        TextField(
+                          controller: _name,
+                        ),
+                        ElevatedButton(
+                            onPressed: () async {
+                              Navigator.pop(context);
                               await Future.delayed(
                                   const Duration(milliseconds: 400));
-                              await _camera!.dispose();
-                              await Future.delayed(
-                                  const Duration(milliseconds: 400));
-                              _camera = null;
-                            }
+                              data[_name.text] = e1;
+                              jsonFile.writeAsStringSync(json.encode(data));
 
-                            initialCamera();
-                            Navigator.pop(context);
+                              if (_camera != null) {
+                                await _camera!.stopImageStream();
+                                await Future.delayed(
+                                    const Duration(milliseconds: 400));
+                                await _camera!.dispose();
+                                await Future.delayed(
+                                    const Duration(milliseconds: 400));
+                                _camera = null;
+                              }
+
+                              initialCamera();
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Save'))
+                      ]),
+                    );
+                  });
+            },
+            child: const Icon(Icons.add)),
+        body: Builder(builder: (context) {
+          if ((_camera == null || !_camera!.value.isInitialized) || loading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Container(
+            constraints: const BoxConstraints.expand(),
+            padding: EdgeInsets.only(top: 0, bottom: 0),
+            child: _camera == null
+                ? const Center(child: SizedBox())
+                : Stack(
+                    fit: StackFit.expand,
+                    children: <Widget>[
+                      CameraPreview(_camera!),
+                      _buildResults(),
+                      Positioned(
+                        bottom: 20,
+                        child: RawMaterialButton(
+                          onPressed: () {
+                            _getCurrentLocation().then((value) {
+                              globalLat = '${value.latitude}';
+                              globalLong = '${value.longitude}';
+                              getLocationName();
+                              setState(() {
+                                message = 'Latitude $globalLat, Longitude: $globalLong';
+                              });
+                              _liveLocation();
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const AttendanceFormPage(),
+                                ),
+                              );
+                            });
                           },
-                          child: const Text('Save'))
-                    ]),
-                  );
-                });
-          },
-          child: const Icon(Icons.add)),
-      body: Builder(builder: (context) {
-        if ((_camera == null || !_camera!.value.isInitialized) || loading) {
-          return const Center(
-            child: CircularProgressIndicator(),
+                          shape: CircleBorder(
+                            side: BorderSide(color: Colors.white, width: 2.0),
+                          ),
+                          elevation: 2.0,
+                          fillColor: Colors.transparent,
+                          padding: EdgeInsets.all(1),
+                          child: Icon(
+                            Icons.circle,
+                            color: AppColors.mainGreen,
+                            size: 60,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
           );
-        }
-        return Container(
-          constraints: const BoxConstraints.expand(),
-          padding: EdgeInsets.only(
-              top: 0, bottom: MediaQuery.of(context).size.height * 0.2),
-          child: _camera == null
-              ? const Center(child: SizedBox())
-              : Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    CameraPreview(_camera!),
-                    _buildResults(),
-                  ],
-                ),
-        );
-      }),
-    );
+        }));
   }
 
   Widget _buildResults() {
