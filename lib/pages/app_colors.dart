55f773946f817a58c70ced7cc1569d3d1f16f1c5
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-11-20 08:37:40 +0700

    update app color theme

--- lib/pages/app_colors.dart
+++ lib/pages/app_colors.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color grey = Color(0xFFF5F5F5);
+  static const Color mainGrey = Color(0xFFE8E8E8);
+  static const Color deepGrey = Color(0xFFABABAB);
   static const Color lightGreen = Color(0xFF93B1A6);
   static const Color mainGreen = Color(0xFF5C8374);
   static const Color deepGreen = Color(0xFF183D3D);
