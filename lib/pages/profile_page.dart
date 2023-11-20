9138253e096e404bf5386cba8b94798d372a4165
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-11-20 08:32:12 +0700

    update profile layout

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'welcome_page.dart';
 import 'app_colors.dart';
+import 'welcome_page.dart';
 import '../utils/globals.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -13,164 +13,175 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    final heightSize = 0.09 * MediaQuery.of(context).size.height;
-    final widthSize = 0.35 * MediaQuery.of(context).size.width;
-
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            const ProfileCard(),
-            SizedBox(height: heightSize),
-            Center(
-              child: Stack(
-                alignment: Alignment.center,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30, bottom: 20, top: 70),
+          child: Column(
+            children: [
+              const Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    decoration: const BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.transparent,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 150,
-                    height: 150,
-                    child: CircularProgressIndicator(
-                      value: 0.8,
-                      valueColor: AlwaysStoppedAnimation(AppColors.mainGreen),
-                      backgroundColor: AppColors.grey,
-                      strokeWidth: 10,
-                    ),
-                  ),
-                  const Column(children: [
-                    Text(
-                      '80%',
-                      style: TextStyle(
-                        color: AppColors.mainGreen,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      'Kehadiran',
-                      style: TextStyle(
-                        color: AppColors.mainGreen,
-                        fontSize: 12,
-                      ),
-                    ),
-                  ])
+                  Text('PROFILE', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
                 ],
               ),
-            ),
-            SizedBox(height: heightSize),
-            Container(
-              margin: EdgeInsets.symmetric(horizontal: widthSize),
-              child: GestureDetector(
-                onTap: () {
-                  setState(() {
-                    globalLanguage = const Locale('en', ''); 
-                  });
-                  Navigator.push(
-                    context,
-                      MaterialPageRoute(
-                        builder: (context) => const WelcomePage()
-                      )
-                  );
-                },
-                child: Container(
-                  width: double.infinity,
-                  height: 40,
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [Colors.white, AppColors.lightGreen],
+              const SizedBox(height: 10),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Card(
+                    margin: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    borderRadius: BorderRadius.circular(20),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+                    color: AppColors.mainGreen,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const CircleAvatar(
+                                radius: 30,
+                                backgroundImage: AssetImage('assets/avatar.jpg'),
+                              ),
+                              const SizedBox(width: 10),
+                              const Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text('Username', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                                  Text('Mobile Developer', style: TextStyle(color: Colors.white)),
+                                ],
+                              ),
+                              const Spacer(),
+                              InkWell(
+                                onTap: () {
+                                  // Navigator.push(
+                                  //   context,
+                                  //   MaterialPageRoute(builder: (context) => EditProfilePage()),
+                                  // );
+                                },
+                                child: Image.asset(
+                                  'assets/edit.png',
+                                  width: 24, 
+                                  height: 24,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                  child: const Center(
-                    child: Text(
-                      "Log Out",
-                      style: TextStyle(
-                          color: AppColors.deepGreen,
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold),
                     ),
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  Card(
+                    margin: EdgeInsets.zero,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    color: AppColors.grey,
+                    child: Column(
+                      children: [
+                        buildRow('assets/department.png', 'Department', 'IT Department'),
+                        buildDivider(),
+                        buildRow('assets/attendance.png', 'Attendance', 'Performance: 80%'),
+                        buildDivider(),
+                        buildRow('assets/theme.png', 'Swicth Theme', 'Current: Light Mode'),
+                        buildDivider(),
+                        buildRow('assets/language.png', 'Language', 'Current: English'),
+                        // buildDivider(),
+                        // buildRow('assets/logout.png', 'Log Out', 'Log out from your account'),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 75, vertical: 25),
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          globalLanguage = const Locale('en', ''); 
+                        });
+                        Navigator.push(
+                          context,
+                            MaterialPageRoute(
+                              builder: (context) => const WelcomePage()
+                            )
+                        );
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 40,
+                        decoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [Colors.white, AppColors.lightGreen],
+                          ),
+                          borderRadius: BorderRadius.circular(20),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: const Center(
+                          child: Text(
+                            "Log Out",
+                            style: TextStyle(
+                                color: AppColors.mainGreen,
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ),
-            SizedBox(height: heightSize),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-}
 
-class ProfileCard extends StatelessWidget {
-  const ProfileCard({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        color: AppColors.mainGreen,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(20),
-          bottomRight: Radius.circular(20),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 3,
-            offset: Offset(0, 1),
+  Widget buildRow(String imagePath, String text1, String text2) {
+    return Padding(
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: AppColors.mainGrey,
+            radius: 20,
+            child: Padding(
+              padding: const EdgeInsets.all(5),
+              child: CircleAvatar(
+                backgroundColor: AppColors.mainGrey,
+                radius: 15,
+                backgroundImage: AssetImage(imagePath),
+              ),
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(text1, style: const TextStyle(color: AppColors.deepGreen, fontWeight: FontWeight.bold)),
+              Text(text2, style: const TextStyle(color: AppColors.deepGrey)),
+            ],
           ),
         ],
       ),
-      child: Padding(
-        padding: const EdgeInsets.only(top: 100, bottom: 40, left: 20, right: 20),
-        child: Column(
-          children: [
-            Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20), 
-              ),
-              child: Image.asset('assets/avatar.png'), 
-            ),
-            const SizedBox(height: 20),
-            const Text(
-              'Username',
-              style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
-            ),
-            const SizedBox(height: 10),
-            const Text(
-              'Full Stack Mobile Developer',
-              style: TextStyle(fontSize: 14, color: Colors.white),
-            ),
-            const Text(
-              'IT Department',
-              style: TextStyle(fontSize: 14, color: Colors.white),
-            ),
-          ],
-        ),
-      ),
+    );
+  }
+
+  Widget buildDivider() {
+    return const Divider(
+      height: 0.5,
+      color: AppColors.deepGrey,
     );
   }
 }
