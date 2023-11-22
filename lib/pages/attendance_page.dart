7643817ef5f927b4cbad8558ca71147f15890825
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-11-22 13:18:22 +0700

    update attendance list from API data

--- lib/pages/attendance_page.dart
+++ lib/pages/attendance_page.dart
@@ -3,6 +3,7 @@ import 'app_colors.dart';
 import 'attendancecorrect_page.dart';
 import '../utils/localizations.dart';
 import '../utils/globals.dart';
+import '../controllers/attendance_controller.dart';
 
 class AttendancePage extends StatefulWidget {
   const AttendancePage({Key? key}) : super(key: key);
@@ -12,6 +13,14 @@ class AttendancePage extends StatefulWidget {
 }
 
 class _AttendancePageState extends State<AttendancePage> {
+  late AttendanceController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AttendanceController();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,243 +40,143 @@ class _AttendancePageState extends State<AttendancePage> {
           },
         ),
       ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
+      body: FutureBuilder<List<AttendanceData>>(
+        future: controller.futureData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return buildAttendanceList(snapshot.data!);
+          } else if (snapshot.hasError) {
+            return Text('Error: ${snapshot.error}');
+          }
+
+          return const CircularProgressIndicator();
+        },
+      ),
+    );
+  }
+
+  Widget buildAttendanceList(List<AttendanceData> data) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [AppColors.deepGreen, AppColors.lightGreen],
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, index) {
+              return Column(
+                children: [
+                  buildAttendanceCard(data[index]),
+                  const SizedBox(height: 8),
+                ],
+              );
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 16,
+          left: 100,
+          right: 100,
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              gradient: const LinearGradient(
+                colors: [Colors.white, AppColors.lightGreen],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
-                colors: [AppColors.deepGreen, AppColors.lightGreen],
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  blurRadius: 5,
+                  spreadRadius: 2,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const AttendanceCorrectionPage(),
+                  ),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.transparent,
+                shadowColor: Colors.transparent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Text(
+                AppLocalizations(globalLanguage).translate("addCorrection"),
+                style: const TextStyle(
+                  color: AppColors.deepGreen,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16),
-            child: ListView(
-              children: [
-                Card(
-                  color: const Color(0xFFBC5757),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: EdgeInsets.zero,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Card(
-                      margin: EdgeInsets.zero,
-                      color: AppColors.grey,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            const Text("16 October 2023"),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: <Widget>[
-                                Image.asset("assets/checkin.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("08:04"),
-                                const Spacer(),
-                                Image.asset("assets/checkout.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("17:00"),
-                                const SizedBox(width: 20),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAttendanceCard(AttendanceData data) {
+    bool hasTimeData = data.jamMasuk != null && data.jamPulang != null;
+
+    Color cardColor = hasTimeData ? AppColors.mainGreen : const Color(0xFFBC5757);
+
+    return Card(
+      color: cardColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.zero,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 20),
+        child: Card(
+          color: AppColors.grey,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(10),
+              bottomRight: Radius.circular(10),
+            ),
+          ),
+          margin: EdgeInsets.zero,
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(data.tglAbsen),
                 const SizedBox(height: 10),
-                Card(
-                  color: AppColors.mainGreen,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: EdgeInsets.zero,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Card(
-                      margin: EdgeInsets.zero,
-                      color: AppColors.grey,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            const Text("13 October 2023"),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: <Widget>[
-                                Image.asset("assets/checkin.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("07:54"),
-                                const Spacer(),
-                                Image.asset("assets/checkout.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("17:00"),
-                                const SizedBox(width: 20),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Card(
-                  color: AppColors.mainGreen,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: EdgeInsets.zero,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Card(
-                      margin: EdgeInsets.zero,
-                      color: AppColors.grey,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            const Text("12 October 2023"),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: <Widget>[
-                                Image.asset("assets/checkin.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("07:04"),
-                                const Spacer(),
-                                Image.asset("assets/checkout.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("17:00"),
-                                const SizedBox(width: 20),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                Card(
-                  color: AppColors.mainGreen,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  margin: EdgeInsets.zero,
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Card(
-                      margin: EdgeInsets.zero,
-                      color: AppColors.grey,
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.only(
-                          topRight: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(20),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            const Text("11 October 2023"),
-                            const SizedBox(height: 10),
-                            Row(
-                              children: <Widget>[
-                                Image.asset("assets/checkin.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("07:04"),
-                                const Spacer(),
-                                Image.asset("assets/checkout.png", width: 24, height: 24),
-                                const SizedBox(width: 10),
-                                const Text("17:00"),
-                                const SizedBox(width: 20),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                Row(
+                  children: <Widget>[
+                    Image.asset("assets/checkin.png", width: 24, height: 24),
+                    const SizedBox(width: 10),
+                    Text(data.jamMasuk ?? 'N/A⠀'),
+                    const Spacer(),
+                    Image.asset("assets/checkout.png", width: 24, height: 24),
+                    const SizedBox(width: 10),
+                    Text(data.jamPulang ?? 'N/A⠀'),
+                    const SizedBox(width: 20),
+                  ],
                 ),
               ],
             ),
           ),
-          Positioned(
-            bottom: 16,
-            left: 100,
-            right: 100,
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                gradient: const LinearGradient(
-                  colors: [Colors.white, AppColors.lightGreen],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.2),
-                    blurRadius: 5,
-                    spreadRadius: 2,
-                    offset: const Offset(0, 3),
-                  ),
-                ],
-              ),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => const AttendanceCorrectionPage(),
-                    ),
-                  );
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.transparent, 
-                  shadowColor: Colors.transparent, 
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                child: Text(
-                    AppLocalizations(globalLanguage).translate("addCorrection"),
-                    style: const TextStyle(
-                      color: AppColors.deepGreen,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-              ),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
