aa7f080bde6d7b03a16198f6245b527d57d43f20
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-12-04 08:11:21 +0700

    implement SOLID design and language date format logic

--- lib/pages/attendance_page.dart
+++ lib/pages/attendance_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'app_colors.dart';
 import 'attendancecorrect_page.dart';
+import 'home_page.dart';
 import '../utils/localizations.dart';
 import '../utils/globals.dart';
 import '../controllers/attendance_controller.dart';
@@ -36,7 +38,12 @@ class _AttendancePageState extends State<AttendancePage> {
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: AppColors.deepGreen),
           onPressed: () {
-            Navigator.of(context).pop();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomePage()
+              ),
+            );
           },
         ),
       ),
@@ -46,7 +53,7 @@ class _AttendancePageState extends State<AttendancePage> {
           if (snapshot.hasData) {
             return buildAttendanceList(snapshot.data!);
           } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
+            return const Text('No data');
           }
 
           return const CircularProgressIndicator();
@@ -68,13 +75,13 @@ class _AttendancePageState extends State<AttendancePage> {
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: ListView.builder(
             itemCount: data.length,
             itemBuilder: (context, index) {
               return Column(
                 children: [
-                  buildAttendanceCard(data[index]),
+                  buildAttendanceCard(data[index], index, data.length),
                   const SizedBox(height: 8),
                 ],
               );
@@ -132,52 +139,64 @@ class _AttendancePageState extends State<AttendancePage> {
     );
   }
 
-  Widget buildAttendanceCard(AttendanceData data) {
+  Widget buildAttendanceCard(AttendanceData data, int index, int listLength) {
     bool hasTimeData = data.jamMasuk != null || data.jamPulang != null;
 
     Color cardColor = hasTimeData ? AppColors.mainGreen : const Color(0xFFBC5757);
 
-    return Card(
-      color: cardColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10),
-      ),
-      margin: EdgeInsets.zero,
-      child: Padding(
-        padding: const EdgeInsets.only(left: 20),
-        child: Card(
-          color: AppColors.grey,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(10),
-              bottomRight: Radius.circular(10),
-            ),
+    return Column(
+      children: [
+        if (index == 0) const SizedBox(height: 16),
+        Card(
+          color: cardColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
           ),
           margin: EdgeInsets.zero,
           child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(data.tglAbsen),
-                const SizedBox(height: 10),
-                Row(
+            padding: const EdgeInsets.only(left: 20),
+            child: Card(
+              color: AppColors.grey,
+              shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(10),
+                  bottomRight: Radius.circular(10),
+                ),
+              ),
+              margin: EdgeInsets.zero,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    Image.asset("assets/checkin.png", width: 24, height: 24),
-                    const SizedBox(width: 10),
-                    Text(data.jamMasuk ?? 'N/A⠀'),
-                    const Spacer(),
-                    Image.asset("assets/checkout.png", width: 24, height: 24),
-                    const SizedBox(width: 10),
-                    Text(data.jamPulang ?? 'N/A⠀'),
-                    const SizedBox(width: 20),
+                    Text(formatLanguageDate(data.tglAbsen)),
+                    const SizedBox(height: 10),
+                    Row(
+                      children: <Widget>[
+                        Image.asset("assets/checkin.png", width: 24, height: 24),
+                        const SizedBox(width: 10),
+                        Text(data.jamMasuk ?? 'N/A⠀'),
+                        const Spacer(),
+                        Image.asset("assets/checkout.png", width: 24, height: 24),
+                        const SizedBox(width: 10),
+                        Text(data.jamPulang ?? 'N/A⠀'),
+                        const SizedBox(width: 20),
+                      ],
+                    ),
                   ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
-      ),
+        if (index == listLength - 1) const SizedBox(height: 76),
+      ]
     );
+  }
+
+  String formatLanguageDate(String dateString) {
+    final DateTime dateTime = DateTime.parse(dateString);
+    final String formattedDate = DateFormat.yMMMMd(globalLanguage.toLanguageTag()).format(dateTime);
+    return formattedDate;
   }
 }
