b047fb465f928abe3ae9f21cf9fed6232671b60a
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-12-12 08:14:54 +0700

    update global with theme

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,4 +5,5 @@ const String apiBaseUrl = '{API}';
 String globalLat = '';
 String globalLong = '';
 String globalLocationName = '';
+String globalTheme = 'Light Theme';
 Locale globalLanguage = const Locale('en', '');
