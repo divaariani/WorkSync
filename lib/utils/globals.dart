2cdb3c819cd7bd75e51f282f67a65bdc08f4f9d8
Nur Zharifah Pradinda Kusuma <111584111+dindakusumaa@users.noreply.github.com>
2023-12-22 07:49:23 +0700

    Add globalBarcode and apiBaseUrl2 in globals.dart

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -2,7 +2,27 @@ library worksync.globals;
 import 'package:flutter/material.dart';
 
 const String apiBaseUrl = '{API}';
+const String apiBaseUrl2 = '{API}';
 String globalLat = '';
 String globalLong = '';
 String globalLocationName = '';
 Locale globalLanguage = const Locale('en', '');
+
+String globalBarcodeLokasiResult = '';
+String globalBarcodeBarangResult = '';
+String globalBarcodeCheckpointResult = ''; 
+String globalBarcodeScanLokasiResult = '';
+List<String> globalBarcodeBarangResults = [];
+List<String> globalBarcodeCheckpointResults = [];
+
+void setGlobalBarcodeLokasiResult(String barcodeStockResult){
+  globalBarcodeLokasiResult = barcodeStockResult;
+}
+
+void setGlobalBarcodeBarangResult(String barcodeBarangResult) { 
+  globalBarcodeBarangResult = barcodeBarangResult;
+}
+
+void setGlobalBarcodeCheckpointResult(String barcodeCheckpointResult) {
+  globalBarcodeCheckpointResult = barcodeCheckpointResult;
+}
