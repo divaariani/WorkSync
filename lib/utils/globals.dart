84fb2b55ca5eeabe5ed148c54889f96818191c30
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2023-12-22 07:28:00 +0700

    add new global variable

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -5,5 +5,6 @@ const String apiBaseUrl = '{API}';
 String globalLat = '';
 String globalLong = '';
 String globalLocationName = '';
+String globalFaceDetection = 'Not Recognized';
 String globalTheme = 'Light Theme';
 Locale globalLanguage = const Locale('en', 'US');
