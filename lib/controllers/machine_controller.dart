6eca71dab8afc895c1a1be0f409d76b1b3e751e0
Diva Ariani <70867193+divaariani@users.noreply.github.com>
2024-02-08 11:23:50 +0700

    add new controller for machine feature

---
+++ lib/controllers/machine_controller.dart
@@ -0,0 +1,92 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'response_model.dart';
+import '../utils/globals.dart';
+
+class MachineController{
+
+  static Future<ResponseModel> postOperatorInOut({
+    required int idwc,
+    required int userId,
+    required String oprTap,
+    required String tap,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$apiBaseUrl3?function=absensi_operator_id_2'),
+      body: {
+        'idwc': idwc.toString(),
+        'userid': userId.toString(),
+        'oprtap': oprTap,
+        'tap': tap,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body); 
+      return ResponseModel.fromJson(responseData);
+    } else {
+      throw Exception('Failed to post form data');
+    }
+  }
+
+  static Future<ResponseModel> postFormOperator({
+    required int id,
+    required String name,
+    required int userId,
+    required String namaoperator,
+    required String statusmesin,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$apiBaseUrl3?function=get_workcenter_list'),
+      body: {
+        'id': id.toString(),
+        'name': name,
+        'userid': userId.toString(),
+        'namaoperator': namaoperator,
+        'statusmesin': statusmesin,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body); 
+      return ResponseModel.fromJson(responseData);
+    } else {
+      throw Exception('Failed to post form data');
+    }
+  }
+
+  static Future<ResponseModel> postFormMachineState({
+    required int id,
+    required String state,
+    required String timestate,
+  }) async {
+    final response = await http.post(
+      Uri.parse('$apiBaseUrl3?function=insert_machine_status'),
+      body: {
+        'pidworkcenter': id.toString(),
+        'pstatus': state,
+        'pstatustime': timestate,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body); 
+      return ResponseModel.fromJson(responseData);
+    } else {
+      throw Exception('Failed to post form data');
+    }
+  }
+
+  static Future<Map<String, dynamic>> getWorkcenterList() async {
+    final response = await http.get(
+      Uri.parse('$apiBaseUrl3?function=get_workcenter_list'),
+    );
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> responseData = json.decode(response.body);
+      return responseData;
+    } else {
+      throw Exception('Failed to fetch data from API');
+    }
+  }
+}
